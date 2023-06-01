7e0b362978286f3979e354c811b5d77d580ced72
Julián Silveira Annoni <87427372+juliansilveira@users.noreply.github.com>
2023-06-01 16:49:21 -0300

    Add files via upload

--- jss/jugadores.js
+++ jss/jugadores.js
@@ -1,48 +1,42 @@
-// leemos el json
-
 let seleccionados = [];
+
+// Recuperar los datos de jugadores seleccionados del LocalStorage
+const seleccionadosGuardados = JSON.parse(localStorage.getItem('seleccionados'));
+if (seleccionadosGuardados && Array.isArray(seleccionadosGuardados)) {
+  seleccionados = seleccionadosGuardados;
+}
+
 fetch('../jugadores.json')
   .then((res) => res.json())
   .then((data) => {
-
-    let dni = 39028680
-    const { jugadores } = data //tomamos cada uno de los jugadores
-    jugadores.forEach((jugador) => { //recorremos e interactuamos con cada jugador
-
-      dni += 1
-
-      jugador.dni = dni
-
-
+    let dni = 39028680;
+    const { jugadores } = data;
+    jugadores.forEach((jugador) => {
+      dni += 1;
+      jugador.dni = dni;
 
       const filaDeJugadores = document.createElement('tr');
 
       const dniJugador = document.createElement('td');
-      dniJugador.textContent = `${dni}`
+      dniJugador.textContent = `${dni}`;
 
       const apellidoJugador = document.createElement('td');
-      apellidoJugador.textContent = `${jugador.apellido}`
+      apellidoJugador.textContent = `${jugador.apellido}`;
 
       const nombreJugador = document.createElement('td');
-      nombreJugador.textContent = `${jugador.nombre}`
+      nombreJugador.textContent = `${jugador.nombre}`;
 
       const posicionJugador = document.createElement('td');
-      posicionJugador.textContent = `${jugador.posicion}`
+      posicionJugador.textContent = `${jugador.posicion}`;
 
       const apodoJugador = document.createElement('td');
-      apodoJugador.textContent = `${jugador.apodo}`
+      apodoJugador.textContent = `${jugador.apodo}`;
 
       const dorsalJugador = document.createElement('td');
-      dorsalJugador.textContent = `${jugador.dorsal}`
+      dorsalJugador.textContent = `${jugador.dorsal}`;
 
       const pieJugador = document.createElement('td');
-      pieJugador.textContent = `${jugador.pie}`
-
-
-
-
-
-      // localStorage.getItem(`${jugador.dorsal}`)
+      pieJugador.textContent = `${jugador.pie}`;
 
       filaDeJugadores.appendChild(dniJugador);
       filaDeJugadores.appendChild(apellidoJugador);
@@ -52,55 +46,39 @@ fetch('../jugadores.json')
       filaDeJugadores.appendChild(dorsalJugador);
       filaDeJugadores.appendChild(pieJugador);
 
-      let tabla = document.querySelector('table')
+      let tabla = document.querySelector('table');
 
-      tabla.appendChild(filaDeJugadores)
+      tabla.appendChild(filaDeJugadores);
 
-      let botonEditar = document.createElement('button')
-      let botonEliminar = document.createElement('button');
+      let botonEditar = document.createElement('button');
       let botonSeleccionar = document.createElement('button');
 
-
-      filaDeJugadores.appendChild(botonEditar)
+      filaDeJugadores.appendChild(botonEditar);
       botonEditar.innerText = 'Editar';
 
-      filaDeJugadores.appendChild(botonEliminar)
-      botonEliminar.innerText = 'Eliminar';
-
-      filaDeJugadores.appendChild(botonSeleccionar)
-      botonSeleccionar.innerText = 'Seleccionar'
+      filaDeJugadores.appendChild(botonSeleccionar);
+      botonSeleccionar.innerText = 'Seleccionar';
 
       botonEditar.addEventListener('click', () => {
-        console.log('Editado', jugador.nombre)
-      })
-
-      botonEliminar.addEventListener('click', () => {
-        console.log('Eliminado', jugador.nombre);
-
-        const clave = 'seleccionados';
-
-        let seleccionados = JSON.parse(localStorage.getItem(clave)) || [];
-
-        const indice = seleccionados.indexOf(jugador.nombre);
-        if (indice !== -1) {
-          seleccionados.splice(indice, 1);
-        }
-
-        localStorage.setItem(clave, JSON.stringify(seleccionados));
+        console.log('Editado', jugador.nombre);
       });
 
-
       botonSeleccionar.addEventListener('click', () => {
+        if (!seleccionados.includes(jugador.nombre) && seleccionados.length < 11) {
+          const numArquerosSeleccionados = seleccionados.filter((nombre) => {
+            const jugadorSeleccionado = jugadores.find((j) => j.nombre === nombre);
+            return jugadorSeleccionado.posicion === 'Arquero';
+          }).length;
 
-        if (!seleccionados.includes(jugador.nombre)) {
+          if (jugador.posicion === 'Arquero' && numArquerosSeleccionados >= 1) {
+            alert('Ya se ha seleccionado un arquero');
+            return;
+          }
+
           seleccionados.push(jugador.nombre);
           localStorage.setItem('seleccionados', JSON.stringify(seleccionados));
 
-
-          // Crear una nueva fila para el jugador seleccionado
           const filaSeleccionado = document.createElement('tr');
-
-          // Crear las celdas correspondientes con los datos del jugador seleccionado
           const dorsalSeleccionado = document.createElement('td');
           dorsalSeleccionado.textContent = jugador.dorsal;
 
@@ -113,32 +91,76 @@ fetch('../jugadores.json')
           const apellidoSeleccionado = document.createElement('td');
           apellidoSeleccionado.textContent = jugador.apellido;
 
-          const btnEliminarSeleccionado = document.createElement('button')
-          btnEliminarSeleccionado.innerText = 'X'
+          const btnEliminarSeleccionado = document.createElement('button');
+          btnEliminarSeleccionado.innerText = 'X';
 
-          // Agregar las celdas a la fila
+          btnEliminarSeleccionado.addEventListener('click', () => {
+
+            const indice = seleccionados.indexOf(jugador.nombre);
+            if (indice !== -1) {
+              seleccionados.splice(indice, 1);
+              localStorage.setItem('seleccionados', JSON.stringify(seleccionados));
+              filaSeleccionado.remove();
+            }
+          });
+
           filaSeleccionado.appendChild(dorsalSeleccionado);
           filaSeleccionado.appendChild(posicionSeleccionado);
           filaSeleccionado.appendChild(nombreSeleccionado);
           filaSeleccionado.appendChild(apellidoSeleccionado);
           filaSeleccionado.appendChild(btnEliminarSeleccionado);
-          //todo quedamos en que logramos cargar bien el local storage y enviar al otro cuadro de seleccionados 
-          // Agregar la fila a la tabla de jugadores seleccionados
+
           filaSeleccionados.appendChild(filaSeleccionado);
         }
-
       });
     });
+
+    const filaSeleccionados = document.querySelector('.selectos');
+
+    seleccionados.forEach((jugadorSeleccionado) => {
+      const jugador = jugadores.find((j) => j.nombre === jugadorSeleccionado);
+
+      if (jugador) {
+        const filaSeleccionado = document.createElement('tr');
+        const dorsalSeleccionado = document.createElement('td');
+        dorsalSeleccionado.textContent = jugador.dorsal;
+
+        const posicionSeleccionado = document.createElement('td');
+        posicionSeleccionado.textContent = jugador.posicion;
+
+        const nombreSeleccionado = document.createElement('td');
+        nombreSeleccionado.textContent = jugador.nombre;
+
+        const apellidoSeleccionado = document.createElement('td');
+        apellidoSeleccionado.textContent = jugador.apellido;
+
+        const btnEliminarSeleccionado = document.createElement('button');
+        btnEliminarSeleccionado.innerText = 'X';
+
+        btnEliminarSeleccionado.addEventListener('click', () => {
+          console.log('Eliminado', jugador.nombre);
+
+          const indice = seleccionados.indexOf(jugador.nombre);
+          if (indice !== -1) {
+            seleccionados.splice(indice, 1);
+            localStorage.setItem('seleccionados', JSON.stringify(seleccionados));
+            filaSeleccionado.remove();
+          }
+        });
+
+        filaSeleccionado.appendChild(dorsalSeleccionado);
+        filaSeleccionado.appendChild(posicionSeleccionado);
+        filaSeleccionado.appendChild(nombreSeleccionado);
+        filaSeleccionado.appendChild(apellidoSeleccionado);
+        filaSeleccionado.appendChild(btnEliminarSeleccionado);
+
+        filaSeleccionados.appendChild(filaSeleccionado);
+      }
+    });
   })
-  .catch(error => {
-    // Manejo de errores
+  .catch((error) => {
     console.error('Error al leer el archivo JSON:', error);
   });
+ //todo se agrego la funcionalindad de que verifica que haya un arquero y sean hasta un máximo de 11 jugadores
 
-
-const filaSeleccionados = document.querySelector('.selectos');
-
-
-//Todo FALTA LOGRAR EL AWAIT PARA QUE CADA CLICK HAGA RECORRER Y ACTUALIZAR LOS JUGADORES SELECCIONADOS
-// Todo TRATAR DE QUITAR EL JUGADOR DE LA SECCION JUGADORES PARA NO REPETIR O CREAR UNA FORMA DE VERIFICAR QUE NO SE REPITAN LO JUGADORES
-
+ //todo falta agregar la funcion de editar jugador y agregar jugadores nuevos a la lista
